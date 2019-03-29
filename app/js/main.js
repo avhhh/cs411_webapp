7baf25d9d3e9aa10c870cb009d94afe45cebe2e7
Alex Schwartzman <>
2019-03-29 10:53:03 -0400

    Adding front end

---
+++ app/js/main.js
@@ -0,0 +1,85 @@
+
+(function ($) {
+    "use strict";
+
+
+    /*==================================================================
+    [ Focus Contact2 ]*/
+    $('.input2').each(function(){
+        $(this).on('blur', function(){
+            if($(this).val().trim() != "") {
+                $(this).addClass('has-val');
+            }
+            else {
+                $(this).removeClass('has-val');
+            }
+        })    
+    })
+            
+  
+    
+    /*==================================================================
+    [ Validate ]*/
+    /*var zipcode = $('.validate-input input[name="zipcode"]');
+    var cuisine = $('.validate-input input[name="cuisine"]');
+    var message = $('.validate-input textarea[name="message"]');
+
+
+    $('.validate-form').on('submit',function(){
+        var check = true;
+
+        if($(name).val().trim() == ''){
+            showValidate(name);
+            check=false;
+        }
+
+
+        if($(email).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
+            showValidate(email);
+            check=false;
+        }
+
+        if($(message).val().trim() == ''){
+            showValidate(message);
+            check=false;
+        }
+
+        return check;
+    });
+
+
+    $('.validate-form .input2').each(function(){
+        $(this).focus(function(){
+           hideValidate(this);
+       });
+    });
+
+    function showValidate(input) {
+        var thisAlert = $(input).parent();
+
+        $(thisAlert).addClass('alert-validate');
+    }
+
+    function hideValidate(input) {
+        var thisAlert = $(input).parent();
+
+        $(thisAlert).removeClass('alert-validate');
+    }*/
+
+    var zipcode = $('input[name="zipcode"]');
+    var cuisine = $('input[name="cuisine"]');
+
+    $.ajax({
+        method: "POST",
+        url:,
+        data:{
+            zipcode: zipcode,
+            cuisine: cuisine
+        }
+    }).done(function(output){
+        
+    });
+    
+    
+
+})(jQuery);
